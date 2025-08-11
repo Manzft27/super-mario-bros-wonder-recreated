f0f601c4a2a3babd9235fe6e2b2cffc078dddb46
Manzft27 <frankbueno2017@gmail.com>
2025-08-10 21:37:13 -0400

    added press l + r text on main menu

--- assets/scenes/main_menu/main_menu.gd
+++ assets/scenes/main_menu/main_menu.gd
@@ -1,9 +1,10 @@
 extends Control
 
-@export var logo_bounce_length: float
+@export var bounce_length: float
 
 func _ready() -> void:
 	_start_logo_bounce_animation()
+	_start_press_l_plus_r_bounce_animation()
 
 
 func _start_logo_bounce_animation() -> void:
@@ -11,5 +12,14 @@ func _start_logo_bounce_animation() -> void:
 	tween.bind_node($logo)
 	tween.set_parallel(false)
 	tween.set_loops()
-	tween.tween_property($logo, "position:y", -logo_bounce_length, 0.5).as_relative()
-	tween.tween_property($logo, "position:y", logo_bounce_length, 0.5).as_relative()
+	tween.tween_property($logo, "position:y", -bounce_length, 0.5).as_relative()
+	tween.tween_property($logo, "position:y", bounce_length, 0.5).as_relative()
+
+
+func _start_press_l_plus_r_bounce_animation() -> void:
+	var tween = get_tree().create_tween().set_trans(Tween.TRANS_CUBIC)
+	tween.bind_node($"press_l+r")
+	tween.set_parallel(false)
+	tween.set_loops()
+	tween.tween_property($"press_l+r", "position:y", -bounce_length*0.75, 0.5).as_relative()
+	tween.tween_property($"press_l+r", "position:y", bounce_length*0.75, 0.5).as_relative()
